0191cb559aed547f4a5a28734f51a8453338c42e
maxbay <maxmbay@gmail.com>
2016-01-08 22:23:41 -0800

    runs off mbp cam

--- src/foo-bar.py
+++ src/foo-bar.py
@@ -1,41 +1,23 @@
-#!/usr/bin/python
+#!/bin/local/python
 
-import sys
-from PyQt4 import QtGui, QtCore
+import cv2
 
-class Example(QtGui.QWidget):
+LOOPS = 500
 
-    def __init__(self):
-        super(Example, self).__init__()
+cap = cv2.VideoCapture(0)
 
-        self.initUI()
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-    def initUI(self):
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        hbox = QtGui.QHBoxLayout(self)
-        pixmap = QtGui.QPixmap("/home/pi/Desktop/toad.png")
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        lbl = QtGui.QLabel(self)
-        lbl.setPixmap(pixmap)
-
-        hbox.addWidget(lbl)
-        self.setLayout(hbox)
-        self.center()
-        self.setWindowTitle('Just A Picture Of Toad')
-        self.show()
-
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-def main():
-
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
